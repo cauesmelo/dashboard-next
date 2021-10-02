1b6f0bec4cac367c04598b8474215f13e06bd5d5
Cauê Melo <cauesmelo@gmail.com>
2021-10-01 21:18:07 -0300

    refactor: create input component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
@@ -19,33 +20,10 @@ export default function Home() {
       >
         <Stack spacing="4">
 
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-            <Input
-              type="email"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
 
-          <FormControl>
-            <FormLabel htmlFor="email">Senha</FormLabel>
-            <Input
-              type="password"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
+          <Input name="email" type="e-mail" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+
           <Button
             type="submit"
             mt="6"
