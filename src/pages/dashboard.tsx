06e0175ec904e59ab517ecd5b3ad56e92540746c
Cauê Melo <cauesmelo@gmail.com>
2021-10-01 22:17:05 -0300

    feat: add sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,17 @@
 import { Header } from "../components/Header";
 
+import { Flex } from '@chakra-ui/react';
+import { Sidebar } from "../components/Sidebar";
+
 export const Dashboard = () => {
   return (
-    <Header></Header>
+    <Flex direction="column" h="100vh">
+      <Header />
+
+      <Flex w="100%" my="6" maxWidth="1480px" mx="auto" px="6px">
+        <Sidebar />
+      </Flex>
+    </Flex>
   )
 }
 
