990a713ac3f1aaa00ca8ec9184ffd6de18e42c44
Cauê Melo <cauesmelo@gmail.com>
2021-10-01 21:09:55 -0300

    feat: add sign in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,61 @@
+import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+
 export default function Home() {
   return (
-    <h1>Dashboard</h1>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        w="100%"
+        maxWidth="360px"
+        bg="gray.800"
+        p="8"
+        borderRadius="8px"
+        flexDir="column"
+      >
+        <Stack spacing="4">
+
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+            <Input
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="email">Senha</FormLabel>
+            <Input
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{
+                bgColor: 'gray.900'
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            mt="6"
+            colorScheme="pink"
+            size="lg"
+          >
+            Entrar
+          </Button>
+        </Stack>
+      </Flex>
+    </Flex>
   )
 }
