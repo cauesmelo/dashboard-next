fe062f5ca1560f25d334529e595c05c47f7d91d6
Cauê Melo <cauesmelo@gmail.com>
2021-10-03 09:50:29 -0300

    feat: add resp for header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,13 +1,18 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react"
 
-export const Profile = () => {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export const Profile = ({ showProfileData = true }: ProfileProps) => {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Cauê Melo</Text>
-        <Text color="gray.300" fontSize="small">cauesmelo@gmail.com</Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Cauê Melo</Text>
+          <Text color="gray.300" fontSize="small">cauesmelo@gmail.com</Text>
+        </Box>
+      )}
       <Avatar size="md" name="Cauê Melo" src="https://xesque.rocketseat.dev/users/avatar/profile-6dce5bcb-a27c-4fe1-8f88-3d569ee6b1b6.jpg" />
     </Flex>
   )
